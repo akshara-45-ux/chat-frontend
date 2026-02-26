ed8b0dc11c561e200b1b43e2ad6e44ffd474467c
akshara-45-ux <akshara707@gmail.com>
2026-02-26 14:11:01 +0530

    second commit

--- src/component/ChatApp.jsx
+++ src/component/ChatApp.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://dashboard.render.com/web/srv-d6fvt8hr0fns73ejqtq0");
 
 function ChatApp() {
   const [messageInput, setMessageInput] = useState("");
